2f6ae43cb8f4a6e5b5279df89db23caea1dd4116
cristhiangarrix <gironjuarezcristhianjosue5@gmail.com>
2024-11-29 18:07:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Formulario enviado con éxito. ¡Gracias por contactarnos!");
+});
